1daede8ebf7b4bf3d6ee59a78a311cffacaa89b0
Dorian Benech <47485034+xmow49@users.noreply.github.com>
2021-02-26 18:34:22 +0100

    First commit
    V1.3.1

---
+++ pins.h
@@ -0,0 +1,25 @@
+//Ce fichier contient tous les pins
+//Bluetooth:
+
+const int HC06_TX = 9;
+const int HC06_RX = 8;
+
+//Relay
+const int RELAY_PIN = 2;
+
+//Buttons
+const int START_BUTTON = 10;
+const int ALARM_BUTTON = 11;
+
+//LCD
+const int LCD_BACKLIGHT = 13;
+const int LCD_RS = 12;
+const int LCD_E = 11;
+const int LCD_D4 = 7;
+const int LCD_D5 = 6;
+const int LCD_D6 = 5;
+const int LCD_D7 = 4;
+
+
+const int LCD_COLUMN = 16;
+const int LCD_ROW = 2;
